46b44c89c983a62a26b29d2e5275840ba92f63c3
Gatea Larisa <gatealarisa@gmail.com>
2023-01-16 16:21:41 +0200

    Implementing Page Navigation

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 'use strict';
 
-// Modal window
+//Selecting elements
 
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+// Modal window
 
 const openModal = function (e) {
   e.preventDefault();
@@ -30,10 +34,6 @@ document.addEventListener('keydown', function (e) {
 
 // Smooth scrolling
 
-//Selecting elements
-const btnScrollTo = document.querySelector('.btn--scroll-to');
-const section1 = document.querySelector('#section--1');
-
 btnScrollTo.addEventListener('click', function (e) {
   const s1coords = section1.getBoundingClientRect();
 
@@ -52,4 +52,28 @@ btnScrollTo.addEventListener('click', function (e) {
 
   //in modern browsers
   section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+// Page navigation
+
+// document.querySelectorAll('.nav__link').forEach(function (el) {
+//   el.addEventListener('click', function (e) {
+//     e.preventDefault();
+//     const id = this.getAttribute('href');
+//     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+//   });
+// });
+
+//Event Delegation
+//1. Add event listener to common parent element
+//2. Determine what element originated the event
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
+
+  //Matching strategy
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
 });
